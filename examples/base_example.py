52c89a15ed59a6b45bfe48ad00f2bfc4a0db5a58
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-01-27 20:17:26 +0000

    Update example.

--- examples/base_example.py
+++ examples/base_example.py
@@ -25,14 +25,36 @@ def main():
             rdkitmol=rdkitmol,
             conformers=conformers,
         )
-        diameters = mes_mol.get_diameters(
+        ellipsoids = mes_mol.get_ellipsoids(
             boxmargin=4.0,
             vdwscale=0.9,
             spacing=0.5,
         )
+        diameters = {i: ellipsoids[i][1] for i in ellipsoids}
         ratios = mes_mol.get_inertial_ratios()
         mes.plot_shapes(ratios, filename=f'{name}_shape.pdf')
         mes.plot_diameters(diameters, filename=f'{name}_size.pdf')
+
+        # Pick an ellipsoid to draw.
+        cid = 0
+        center, diameter, rotation = ellipsoids[cid]
+        mes_mol.draw_conformer_hitpoints(
+            cid=cid,
+            center=center,
+            diameter=diameter,
+            rotation=rotation,
+            boxmargin=4.0,
+            vdwscale=0.9,
+            spacing=0.5,
+            filename=f'{name}_ellhp.pdf',
+        )
+        fig, ax = mes.plot_ellipsoid(
+            center=center,
+            diameter=diameter,
+            rotation=rotation,
+        )
+        fig.tight_layout()
+        fig.savefig(f'{name}_ell.pdf', dpi=720, bbox_inches='tight')
 
 
 if __name__ == '__main__':
