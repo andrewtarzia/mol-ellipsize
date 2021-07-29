6d7e58ad7c85a42b57d49a40ec4eee21797d0c4e
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-07-29 14:05:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     ),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GPLv3 License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
